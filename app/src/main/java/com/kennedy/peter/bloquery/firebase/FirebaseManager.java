fee5d13bc79ca4d7bc20430d40551899fbfe5a2b
Peter Kennedy <pykennedy@gmail.com>
2016-02-02 23:31:11 -0800

    user can create account and log in

--- app/src/main/java/com/kennedy/peter/bloquery/firebase/FirebaseManager.java
+++ app/src/main/java/com/kennedy/peter/bloquery/firebase/FirebaseManager.java
@@ -5,8 +5,8 @@ import com.firebase.client.Firebase;
 public class FirebaseManager {
     Firebase firebase = new Firebase("https://flickering-torch-9808.firebaseio.com/");
 
-    public void logIn(String username, String password) {
-        firebase.authWithPassword();
+    public void logIn(String username, String password, Firebase.AuthResultHandler handler) {
+        firebase.authWithPassword(username, password, handler);
     }
 
     public void createAccount(String email, String password, Firebase.ResultHandler handler) {
